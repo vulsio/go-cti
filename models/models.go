3c5d1189a9cabba416c3f5feeb95c6306b1d7073
okimoto@cysec.cs.ritsumei.ac.jp <okimoto@cysec.cs.ritsumei.ac.jp>
2021-07-09 15:04:46 +0900

    feat(search): Removed unnecessary member ID and CtiID

--- models/models.go
+++ models/models.go
@@ -11,22 +11,20 @@ type LastUpdated struct {
 
 // Cti : Cyber Threat Intelligence
 type Cti struct {
-	ID               int64 `json:",omitempty"`
-	Name             string
-	Type             string
-	Description      string
-	CveID            string
-	Capec            *Capec      `json:",omitempty"`
-	KillChains       []KillChain `json:",omitempty"`
-	References       []Reference `json:",omitempty"`
+	ID          int64 `json:",omitempty"`
+	Name        string
+	Type        string
+	Description string
+	CveID       string
+	Capec       *Capec
+	KillChains  []KillChain
+	References  []Reference
 	// PublishedDate    time.Time
 	// LastModifiedDate time.Time
 }
 
 // Capec is Child model of Cti
 type Capec struct {
-	ID       int64 `json:",omitempty"`
-	CtiID    int64 `sql:"type:bigint REFERENCES ctis(id)" json:",omitempty"`
 	Abstruct string
 	Status   string
 	Severity string
@@ -36,16 +34,12 @@ type Capec struct {
 
 // KillChain is Child model of Cti
 type KillChain struct {
-	ID    int64 `json:",omitempty"`
-	CtiID int64 `sql:"type:bigint REFERENCES ctis(id)" json:",omitempty"`
 	Name  string
 	Phase string
 }
 
 // Reference is Child model of Cti
 type Reference struct {
-	ID          int64 `json:",omitempty"`
-	CtiID       int64 `sql:"type:bigint REFERENCES ctis(id)" json:",omitempty"`
 	ExternalID  string
 	Link        string `sql:"type:text"`
 	Description string `sql:"type:text"`
